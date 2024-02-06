c4271ba02a803860ec2365f459868f72acaa1629
Ted Kouhouenou <teddams047@gmail.com>
2024-02-06 16:08:49 +0100

    Getting started with conditionals

---
+++ Getting-started-with-conditionals/conditionnal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -n 1 char
+if [[ $char == 'Y' || $char == 'y' ]]; then
+    echo "YES"
+elif [[ $char == 'N' || $char == 'n' ]]; then
+    echo "NO"
+else
+    echo "Enter Only char between Y/y or N/n"
+fi
+
+
