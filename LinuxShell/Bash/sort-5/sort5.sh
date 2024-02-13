1aab00f2fe44f0fa8d1562d8968c428bf915f44a
Ted Kouhouenou <teddams047@gmail.com>
2024-02-13 22:49:39 +0100

    sort 5

---
+++ LinuxShell/Bash/sort-5/sort5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sort -t $'\t' -k 2 -nr 
+
+# -t : delimiter the $ is used for precise that is the a special characters
+# -k : specify field 
+# -n ; for precise the numerical numbr 
+# -r for make a reverse of output 
